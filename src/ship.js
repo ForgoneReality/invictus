cd2552f7e046c42c70c625432d3078c9b88afd34
Cao Xu <46094706+ForgoneReality@users.noreply.github.com>
2022-09-07 20:16:05 -0700

    refixing for portfolio

--- src/ship.js
+++ src/ship.js
@@ -108,7 +108,7 @@ const TYPES = [
 
         velocity: [0, 0.5],
         endvelocity: [0, 0],
-        health: 80,
+        health: 8000,
         damage: 80, //variable!
         img: 'images/boss1.png',
         color: "red",
